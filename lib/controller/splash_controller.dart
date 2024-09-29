d01158e6d553f674f8274909357a22be90cfea16
Oualid Rouabah <oualidrouabah56@gmail.com>
2024-09-29 17:10:58 +0100

    add aplash screen

---
+++ lib/controller/splash_controller.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:get/get.dart';
+
+import '../view/tashkil_page.dart';
+
+class SplashController extends GetxController {
+  @override
+  void onInit() {
+    super.onInit();
+    // Navigate to the home page after 4 seconds
+    Timer(Duration(seconds: 4), () {
+      Get.off(TashkilPage());
+    });
+  }
+}
