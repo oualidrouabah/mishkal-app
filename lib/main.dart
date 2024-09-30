176f2292b7683be2780a388fe80b6a03b93096c8
Oualid Rouabah <oualidrouabah56@gmail.com>
2024-09-30 12:13:15 +0100

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mishkal/view/home.dart';
+import 'package:mishkal/view/splash_screen.dart';
 import 'package:mishkal/view/theme.dart';
 
 import 'controller/tashkil_controller.dart';
@@ -12,11 +12,11 @@ void main() {
 
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: HomePage(),
+      home: SplashScreen(),
       title: "Mishkal app",
       locale:const Locale('ar', 'DZ'),
       fallbackLocale: const Locale('en', 'US'),
