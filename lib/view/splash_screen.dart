7d0032e4600f7b50202712b21a53eab0f2234e68
Oualid Rouabah <oualidrouabah56@gmail.com>
2024-09-30 12:15:29 +0100

    add splash screen

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controller/splash_controller.dart';
 
-class SplashScreen extends StatelessWidget{
+class SplashScreen extends StatelessWidget {
+  SplashScreen({super.key});
+
+  final SplashController controller = Get.put(SplashController());
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Center(
+        child: AnimatedSplashImage(),
+      ),
+    );
   }
-  
+}
+
+class AnimatedSplashImage extends StatefulWidget {
+  const AnimatedSplashImage({super.key});
+
+  @override
+  _AnimatedSplashImageState createState() => _AnimatedSplashImageState();
+}
+
+class _AnimatedSplashImageState extends State<AnimatedSplashImage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    )..repeat(reverse: true);
+    _animation = Tween<double>(begin: 0.5, end: 1.0).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _animation,
+      child: Image.asset('assets/mishkalphoto.png', scale: 0.5),
+    );
+  }
 }
